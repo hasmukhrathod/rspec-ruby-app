7d89f5d35e4716a40e4006b5df5567782ce48736
Hasmukh <hasmukhrathod@gmail.com>
2018-12-29 11:36:48 +0000

    factorial nil condition covered.

--- spec/factorial/factorial.rb
+++ spec/factorial/factorial.rb
@@ -1,9 +1,13 @@
 class Factorial
   def get_factorial(number:)
-    arr_dp = [1]
-    for i in 1..number do
-      arr_dp[i] = i * arr_dp[i-1]
-    end
-    return arr_dp[number]
+    if number >= 0
+      arr_dp = [1]
+      for i in 1..number do
+        arr_dp[i] = i * arr_dp[i-1]
+      end
+      return arr_dp[number]
+    else
+      return nil
+    end  
   end
 end
--- spec/factorial/factorial_spec.rb
+++ spec/factorial/factorial_spec.rb
@@ -3,8 +3,14 @@ require 'factorial/factorial'
 RSpec.describe Factorial do
   let(:inst_obj_factorial) {Factorial.new}
   describe "#get_factorial" do
+    it 'factorial of negative no is nil' do
+      expect(inst_obj_factorial.get_factorial(number: -2)).to be_nil
+    end
     it 'factorial of 5 should be 120' do
       expect(inst_obj_factorial.get_factorial(number: 5)).to eq(120)
+    end
+    it 'factorial of 6 should not be 120' do
+      expect(inst_obj_factorial.get_factorial(number: 6)).not_to eq(700)
     end
   end
 end
