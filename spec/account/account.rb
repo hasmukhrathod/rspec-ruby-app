eda1b05772d1a0792fd7083898164efd9bd391a5
Hasmukh <hasmukhrathod@gmail.com>
2019-01-04 08:14:12 +0000

    transcation added.

--- spec/account/account.rb
+++ spec/account/account.rb
@@ -1,11 +1,24 @@
 class Account
   attr_accessor :balance
+  
   def initialize(balance: 0)
     @balance = balance
   end
   
   def current_balance
     @balance
+  end
+  
+  def transaction(type:, amount:)
+    if type == 'deposite'
+      @balance+= amount
+    else
+      if @balance < amount
+        puts nil
+      else
+        @balance-= amount
+      end
+    end
   end
   
 end
--- spec/account/account_spec.rb
+++ spec/account/account_spec.rb
@@ -8,16 +8,25 @@ RSpec.describe Account do
       it 'should have balance 0' do
         should eq(0)
       end
-      
     end
-    
     context "Open an account with 1000" do
       let(:account) {Account.new(balance: 1000)}
       subject {account.current_balance}
-      
       it 'should have balance 1000' do
         should eq(1000)
       end
     end
   end
+  describe '#transaction' do
+    let(:account) {Account.new}
+    it "should add balance to current amount" do
+      #should eq(account.transaction(type: 'deposite',amount: 1000))
+      expect(account.transaction(type: 'deposite', amount: 500)).to eq(500)
+      expect(account.transaction(type: 'withdrawn', amount: 500)).to eq(0)
+      expect(account.transaction(type: 'withdrawn', amount: 500)).to be_nil
+    end
+    
+    
+  end
+  
 end
